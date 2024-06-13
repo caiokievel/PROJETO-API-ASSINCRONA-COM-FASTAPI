db9806a268a429a349f8f9f89857bc2886c5f9ef
Caio <caaiokievel@gmail.com>
2024-06-13 15:49:54 -0300

    correcao env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,7 @@
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import Connection
+from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy import pool
 
