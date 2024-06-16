15659aba58537fdbd23bf594fb6b78cef21a5bfb
Caio <caaiokievel@gmail.com>
2024-06-15 21:25:30 -0300

    ajuste imports

--- workout_api/atleta/models.py
+++ workout_api/atleta/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workout_api.categorias.models import CategoriaModel
+from workout_api.centro_treinamento.models import CentroTreinamentoModel
 from workout_api.contrib.models import BaseModel
 
 
